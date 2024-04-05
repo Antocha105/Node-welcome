ec5adc72c55af7dc850e940518fc74259b953294
Chuprin Anton <workchuprin105@gmail.com>
2024-04-05 21:35:12 +0300

    apeend file

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const fs = require('fs').promises
 
 fs.readFile('./text.txt', 'utf-8')
 .then(content=>{
-    const newContent = `Old content: ${content} and new content: 'Hello world'`;
-    fs.writeFile('./newFile.txt', newContent, 'utf8')
+    const newContent = `APPEND Old content: ${content} and new content: 'Hello world'`;
+    //fs.writeFile('./newFile.txt', newContent, 'utf8')   //Перезаписує файли
+
+    fs.appendFile('./newFile.txt',newContent, 'utf8') //Дописує файли
 })
