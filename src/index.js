e7fd51454040ef9501d23f19a02b78f597f4d8b8
Chuprin Anton <workchuprin105@gmail.com>
2024-04-07 09:41:52 +0300

    web pack

--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ const _ = require('lodash')
 const randomNumber = Number(_.random(1,10,false).toFixed(2))
 
 console.log(randomNumber);
+
+const div = document.querySelector('#root')
+
+div.append('HELLO WEBPACK')
